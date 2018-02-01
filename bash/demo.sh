a82dcf64f18b50e2b70795c626766b319b3f2429
babu <babusarvisetty99@gmail.com>
2018-02-01 11:35:08 +0530

    this is bash script

---
+++ bash/demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "this is values change by baubu"
+
+echo "please enter the value"
+
+read valu
+
+echo "this id values of variable $valu"
+
+if [ $l -gt 10 ]
+
+then
+
+echo "this is number intialization"
+
+
+fi
+
+
